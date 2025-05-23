5855d4b450388286766c5779997dbdd557bdcbc1
hana1101 <monniej01@gmail.com>
2025-05-23 16:51:42 +0900

    yo

---
+++ sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(400, 400);
+  textSize(20)
+  textAlign(CENTER,CENTER)
+
+}
+
+function draw() {
+  background(220);
+  text('GITHUB 배표 테스트', width/2, height/2)
+}
