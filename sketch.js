34196d682870ce4094430ee0a509e980de3e1164
hana1101 <monniej01@gmail.com>
2025-05-23 16:52:47 +0900

    add console.log(123)

--- sketch.js
+++ sketch.js
@@ -2,10 +2,12 @@ function setup() {
   createCanvas(400, 400);
   textSize(20)
   textAlign(CENTER,CENTER)
-
+  console .log(123)
+  
 }
 
 function draw() {
   background(220);
   text('GITHUB 배표 테스트', width/2, height/2)
+
 }
